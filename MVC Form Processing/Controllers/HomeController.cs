0b0dc49b702de6d17e8665652ebbb33b9a1cb80b
Salvatore DiBenedetto <SalvatoreDiBenedetto94@outlook.com>
2017-02-21 23:44:40 -0500

    Add project files.

---
+++ MVC Form Processing/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Form_Processing.Models;
+
+namespace MVC_Form_Processing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			var greeting = "";
+			var time = DateTime.Now;
+			if (time.Hour < 12)
+			{
+				greeting = "Good morning!";
+			}
+			else if (time.Hour >= 12 && time.Hour < 18)
+			{
+				greeting = "Good Afternoon!";
+			}
+			else
+			{
+				greeting = "Good Evening!";
+			}
+
+			var shortTime = time.ToString("hh:mm tt");
+			var day = time.DayOfWeek.ToString();
+			var month = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(time.Month);
+
+			int daysInYear = DateTime.IsLeapYear(time.Year) ? 366 : 365;
+			var daysleft = daysInYear - time.DayOfYear;
+
+			
+			ViewData["greeting"] = greeting;
+			ViewData["shortTime"] = shortTime;
+			ViewData["day"] = day;
+			ViewData["dayofmonth"] = time.Day;
+			ViewData["month"] = month;
+			ViewData["year"] = time.Year;
+			ViewData["daysleft"] = daysleft;
+
+			return View();
+        }
+
+        public IActionResult ShowPerson()
+        {
+			Person person = new Person();
+			person.firstName = "Michael";
+			person.lastName = "Holder";
+			person.birthDate = DateTime.ParseExact("24/01/2000", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+			person.age = 17;
+
+			ViewBag.Message = "Showing Person Details.";
+			ViewBag.Person = person;
+
+			return View(); 
+        }
+				
+		
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+		
+		
+		[HttpGet]
+		public IActionResult AddPerson()		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult AddPerson(Person person)
+		{
+			if (ModelState.IsValid)
+			{
+				ViewBag.Message = "Showing details of " + person.firstName;
+				ViewBag.Person = person;
+				return View("ShowPerson");
+			}
+			return View();
+		}
+	}
+}
---
+++ MVC Form Processing/Models/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Form_Processing.Models
+{
+    public class Person
+    {
+		[StringLength(20, MinimumLength = 2)]
+		[Display(Name = "First Name")]
+		[Required(ErrorMessage = "This is a required field")]
+		public String firstName
+		{
+			get; set;
+		}
+
+		[StringLength(20, MinimumLength = 2)]
+		[Display(Name = "Last Name")]
+		[Required(ErrorMessage = "This is a required field")]
+		public String lastName
+		{
+			get; set;
+		}
+
+		[Display(Name = "Date of Birth"), DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+		[Required(ErrorMessage = "This is a required field")]
+		[DataType(DataType.DateTime)]
+		public DateTime birthDate
+		{
+			get; set;
+		}
+
+		[Display(Name = "Age")]
+		[Required(ErrorMessage = "This is a required field")]
+		[DataType(DataType.Currency)]
+		public int age
+		{
+			get; set;
+		}
+
+    }
+}
